55ae9f849e7601e5dc7003424f4b677a3e79e85e
Andre Loureiro <andreloureiroo@gmail.com>
2016-04-13 02:40:12 -0300

    Delete update script

--- update-demo.sh
+++
@@ -1,9 +0,0 @@
-git checkout gh-pages
-git rebase master
-boot build
-cp -R target/main.js target/index.html target/css target/img .
-git add .
-git commit -m "Update demo page"
-git push origin gh-pages
-git checkout master
-
